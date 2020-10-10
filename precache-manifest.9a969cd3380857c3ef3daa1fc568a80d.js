5c0e9d240e84171ab6f940a2d24b703b2e8412d0
Khizhniak <ykhizhnyak@gmail.com>
2020-10-10 11:03:01 +0300

    Updates

---
+++ precache-manifest.9a969cd3380857c3ef3daa1fc568a80d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f2839c45af539055cc4b0a4a3e123d",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "218f637240b0d99f4656",
+    "url": "/goit-react-hw-02-feedback/static/css/main.60877b05.chunk.css"
+  },
+  {
+    "revision": "ac82073cb66cc75bdf96",
+    "url": "/goit-react-hw-02-feedback/static/js/2.563a0764.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.563a0764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218f637240b0d99f4656",
+    "url": "/goit-react-hw-02-feedback/static/js/main.03fad8be.chunk.js"
+  },
+  {
+    "revision": "89db0fb1149f0469c2be",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.a3d6655d.js"
+  }
+]);
